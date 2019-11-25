39a6aa05151239bdb6c30f0186186c5950008d9f
jpastorm <jospastorm@upt.pe>
2019-11-25 00:26:03 -0500

    Diseño ticket admin

--- ticketadmin.py
+++ ticketadmin.py
@@ -7,3 +7,9 @@ def ticketadmin():
 	lista_tickets=ticket_collection.find()
 	lista_asunto=asunto_collection.find()
 	return render_template('ticketadmin.html',tickets=lista_tickets,asuntos=lista_asunto)
+
+@app.route('/ticketselect/<numero>',methods=['POST','GET'])
+def ticketselect(numero):
+	ticket_collection=mongo.db.ticket
+	lista_tickets=ticket_collection.find_one({'numero':numero})
+	return render_template('ticketselect.html',ticket=lista_tickets)
