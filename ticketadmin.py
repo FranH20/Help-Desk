0464531e97f333591e0a20d7a86d9012d4e0e1df
jpastorm <jospastorm@upt.pe>
2019-11-25 13:08:04 -0500

    Bad request en ticketreply

--- ticketadmin.py
+++ ticketadmin.py
@@ -11,5 +11,27 @@ def ticketadmin():
 @app.route('/ticketselect/<numero>',methods=['POST','GET'])
 def ticketselect(numero):
 	ticket_collection=mongo.db.ticket
+	tema_collection=mongo.db.temas
 	lista_tickets=ticket_collection.find_one({'numero':numero})
-	return render_template('ticketselect.html',ticket=lista_tickets)
+	lista_tema=tema_collection.find()
+	return render_template('ticketselect.html',ticket=lista_tickets,temas=lista_tema)
+
+@app.route('/ticketreply',methods=['POST'])
+def ticketreply():
+	ticketxreply_collection=mongo.db.ticketxreply
+	admin_collection=mongo.db.admin
+	ticket_collection=mongo.db.ticket
+	user_find=admin_collection.find_one({'emailadmin':"jose@jose.com"})
+	idadmin=user_find['_id']
+	tipoadmin=user_find['adminroll']
+	respuestafecha=datetime.datetime.today()
+	estado=request.form['estado']
+	descripcion=request.form['descripcion']
+	idticket=request.form['idticket']
+	archivo = request.files['archivo']
+	mongo.save_file(archivo.filename, archivo)
+	ticketxreply_collection.insert({'idticket':idticket,'id_admin':idadmin,'tipo_admin':tipoadmin,'reply_date':respuestafecha,'estado':estado,'descripcion':descripcion,'archivo':archivo.filename})
+	ticket_id=ticket_collection.find_one({'_id':idticket})
+	if ticket_id['asignado'] is None:
+		ticket_collection.find_one_and_update({"_id" : idticket},{"$set":{"asignado": idadmin}},upsert=True)
+	return ticketadmin()
