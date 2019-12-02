068d23e4ea5de37d794cb246f0eda7cc8e25efac
jpastorm <jospastorm@upt.pe>
2019-12-01 20:27:11 -0500

    respuesta de usuario al ticket, problemas en el form de ticketselectuser

--- ticket.py
+++ ticket.py
@@ -25,10 +25,47 @@ def generarticket():
 def cargarticketespera():
     ticket_collection=mongo.db.ticket
     tema_collection=mongo.db.temas
+    asunto_collection=mongo.db.asunto
     lista_temas=tema_collection.find()
     ticket_all = ticket_collection.find({'emailuser': session['emailuser'],'estado':"A"})
-    return render_template('ticketespera.html',datos=ticket_all,temas=lista_temas)
+    #
+    lista_tickets=ticket_collection.find()
+    lista_asunto=asunto_collection.find()
+    return render_template('ticketespera.html',tickets=lista_tickets,asuntos=lista_asunto)
+    #return render_template('ticketespera.html',datos=ticket_all,temas=lista_temas)
 
 @app.route('/file/<filename>')
 def file(filename):
     return mongo.send_file(filename)
+
+@app.route('/ticketselectuser/<numero>/<id>',methods=['POST','GET'])
+def ticketselectuser(numero,id):
+    ticket_collection=mongo.db.ticket
+    tema_collection=mongo.db.temas
+    admin_collection=mongo.db.admin
+    lista_tickets=ticket_collection.find_one({'numero':numero})
+    lista_tema=tema_collection.find()
+    hilo_ticket=mongo.db.ticketxreply
+    hilo_list=hilo_ticket.find({'idticket':id})
+    idadmin=lista_tickets['asignado']
+    list_user=admin_collection.find_one({'_id':idadmin})
+    return render_template('ticketselect.html',ticket=lista_tickets,temas=lista_tema,listhilo=hilo_list,usuarios=list_user)
+
+
+@app.route('/ticketreplyuser',methods=['POST'])
+def ticketreplyuser():
+    ticketxreply_collection=mongo.db.ticketxreply
+    admin_collection=mongo.db.admin
+    user_collection=mongo.db.admin
+    ticket_collection=mongo.db.ticket
+    user_find=user_collection.find_one({'emailuser':session['emailuser']})
+    idadmin=session['emailuser']
+    tipoadmin="user"
+    respuestafecha=datetime.datetime.today()
+    estado=request.form['estado']
+    descripcion=request.form['descripcion']
+    idticket=request.form['idticket']
+    archivo = request.files['archivo']
+    mongo.save_file(archivo.filename, archivo)
+    ticketxreply_collection.insert({'idticket':idticket,'id_admin':idadmin,'tipo_admin':tipoadmin,'reply_date':respuestafecha,'estado':estado,'descripcion':descripcion,'archivo':archivo.filename})    
+    return cargarticketespera()
