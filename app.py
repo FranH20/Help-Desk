8411010cff7f062bf1e8997c8951254e1c0e41e8
Franklin Huichi <carloshuichi@gmail.com>
2021-09-04 22:54:44 -0500

    Limpiando

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-from flask import Flask, render_template, url_for, request, session, redirect,flash,logging
+from flask import Flask,render_template
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-import bcrypt
-import datetime
 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['MONGO_DBNAME'] = 'helpdesk'
-app.config['MONGO_URI'] = 'mongodb+srv://test:test@episupt-tasga.gcp.mongodb.net/helpdesk?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = 'mongodb://localhost/helpdesk'
 
 mongo = PyMongo(app)
-
 
 from login import *
 from profile import *
@@ -21,10 +17,9 @@ from ticketadmin import *
 from incidencias import *
 from usuarioincidencia import*
 
-#@app.route('/')
-#def index():
- #   return render_template('index.html')
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
--- login.py
+++ login.py
@@ -1,9 +1,5 @@
 from app import *
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+from flask import Flask,render_template
 
 @app.route('/profile')
 def profile():
